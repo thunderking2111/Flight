f4a3dd760f278825d8fc270fc6c4380682b33bd1
Rahul Prajapati <rrahulp395@gmail.com>
2024-01-19 00:47:13 +0530

    Migrate to PostGreSql

--- capstone/settings.py
+++ capstone/settings.py
@@ -87,8 +87,11 @@ INTERNAL_IPS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'flight_django',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
     }
 }
 
